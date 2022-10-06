e266725d0e7241642dfc93678a436a00e07a9ef2
unknown <Administrateur@TURING-B22.formationara.local>
2022-10-06 14:33:50 +0200

    merci nathan

--- app-test/app/route.js
+++ app-test/app/route.js
@@ -1,6 +1,38 @@
 const formValidator = require('./form_validator');
 const photoModel = require('./photo_model');
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
 
+async function quickstart(
+  projectId = 'leadtechnique2022', // Your Google Cloud Platform project ID
+  topicNameOrId = 'dmi2-9', // Name for the new topic to create
+  subscriptionName = 'dmi2-8' // Name for the new subscription to create
+) {
+  // Instantiates a client
+  const pubsub = new PubSub({projectId});
+
+  // Creates a new topic
+  const topic = await pubsub.topic(topicNameOrId);
+  console.log(`Topic ${topic.name} retrieved.`);
+
+  // // Creates a subscription on that new topic
+  const subscription = await topic.subscription(subscriptionName);
+
+  // Receive callbacks for new messages on the subscription
+  subscription.on('message', message => {
+    console.log('Received message:', message.data.toString());
+    process.exit(0);
+  });
+
+  // Receive callbacks for errors on the subscription
+  subscription.on('error', error => {
+    console.error('Received error:', error);
+    process.exit(1);
+  });
+
+  // // Send a message to the topic
+  topic.publish(Buffer.from('*sending* ondes venues d"un autre monde!'));
+}
 function route(app) {
   app.get('/', (req, res) => {
     const tags = req.query.tags;
@@ -31,11 +63,18 @@ function route(app) {
       .then(photos => {
         ejsLocalVariables.photos = photos;
         ejsLocalVariables.searchResults = true;
+        ejsLocalVariables.tags = tags
         return res.render('index', ejsLocalVariables);
       })
       .catch(error => {
         return res.status(500).send({ error });
       });
+  });
+  app.post('/zip', function (req, res) {
+    const tags = req.query.tags; 
+    console.log(tags);
+    quickstart()
+    
   });
 }
 
--- app-test/app/server.js
+++ app-test/app/server.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
+const consumer = require("./queue.js");
+const { json } = require('express');
 
 const app = express();
-
 // public assets
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
@@ -21,5 +23,4 @@ require('./route')(app);
 const port = process.env.PORT || 3000;
 app.server = app.listen(port);
 console.log(`listening on port ${port}`);
-
 module.exports = app;
